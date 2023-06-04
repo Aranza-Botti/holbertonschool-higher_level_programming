1083b4085120d58d6873aa0206113bbe65c98196
Aranza <aranza.botti@gmail.com>
2023-06-03 17:07:53 -0700

    4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-def main():
-    l = dir(hidden_4)
-    for i in range(len(l)):
-        if(l[i][0] != '_'):
-            print("{}".format(l[i]))
 if __name__ == "__main__":
-    main()
+import hidden_4
+for a in dir(hidden_4):
+if a[:2] != "__":
+print("{}".format(a))
