f863f60292b632027cfd68691afa7bdcbb7c0264
Aranza <aranza.botti@gmail.com>
2023-06-11 20:27:05 -0700

    2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def divisible_by_2(my_list=[]):
     """
     find the values divisible by two in list
@@ -7,10 +6,8 @@ def divisible_by_2(my_list=[]):
         my_list - list to search
     Return:
         list of True or False
-    """    
+    """
     return list(map(lambda x: False if x % 2 else True, my_list))
-
-
 if __name__ == '__main__':
     tmp = list(range(25))
     tmp2 = divisible_by_2(tmp)
