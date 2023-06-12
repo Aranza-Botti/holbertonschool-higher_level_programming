0bba656d83a9182fcee81d37e02e10e9b4a7822a
Aranza <aranza.botti@gmail.com>
2023-06-11 19:34:59 -0700

    1

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -9,7 +9,7 @@ def delete_at(my_list=[], idx=0):
         my_list - if idx is out of range
     """
     if -1 < idx < len(my_list):
-        del my_list[idx]
+        del my_list[idx]        
     return my_list
 if __name__ == '__main__':
     lst = list(range(1, 6))
